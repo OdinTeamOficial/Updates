6afdc1c89c23b0e456456c5f58143abc58bfdaa1
OdinTeamOficial <OdinRP06@gmail.com>
2021-04-07 17:17:32 -0300

    Release

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'OdinTeam'
+contact 'Discord: https://discord.gg/p9vq7U2qTe'
+version '2.1'
+
+
+client_scripts {
+    "@vrp/lib/utils.lua",
+    "config.lua",
+    "client.lua"
+}
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+	"config.lua",
+	"checkversion.lua",
+	"server.lua"
+}
